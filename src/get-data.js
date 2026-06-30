3f8ebe6568d86791169526a13732f74dc8ea57c2
Pablo Murga <pmurgapita@gmail.com>
2026-06-30 18:26:00 +0200

    Added the loading logo while fetching API
    I added the logo having it hidden and showing at load.

--- src/get-data.js
+++ src/get-data.js
@@ -1,12 +1,16 @@
 
+const load = document.querySelector(".load");
 
 async function getWeather(location, units) {
+    load.removeAttribute('hidden');
     try {
         const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=${units}&key=AGKTW5FXBMRCVQGNEYTHZ83JR`);
         const weatherData = await response.json();
+        load.setAttribute('hidden', 'true');
         return weatherData
     } catch (error) {
         console.error(error);
+        load.setAttribute('hidden', 'true');
     }
     
 }
